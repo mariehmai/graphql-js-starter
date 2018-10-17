0f4e636bd36f5d02dc2f2fb9ebdfdda881f4e06c
mariehmai <mai.mariehelene@gmail.com>
2018-10-17 07:50:58 +0200

    feat(Event): add Event typeDefs and resolvers

--- src/index.js
+++ src/index.js
@@ -1,19 +1,42 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-  info: String!
-}
-`
+let events = [
+  {
+    id: 'event-1',
+    name: 'Semi-finals Volleyball World Cup',
+    location: 'Troyes, FR'
+  },
+  {
+    id: 'event-2',
+    name: 'Finals Volleyball World Cup',
+    description: 'Finals France - Croatia',
+    location: 'Bruxelles, BE'
+  }
+]
+
+let idCount = events.length
 
 const resolvers = {
   Query: {
-    info: () => `GraphQL JS Starter`
+    info: () => `GraphQL JS Starter`,
+    event: () => events
+  },
+  Mutation: {
+    post: (root, args) => {
+      const event = {
+        id: `event-${idCount++}`,
+        name: args.name,
+        description: args.description,
+        location: args.location
+      }
+      events.push(event)
+      return event
+    }
   }
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
