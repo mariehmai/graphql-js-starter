f965f35d5cb1663cf70c2c4d30db4213e85069e8
mariehmai <mai.mariehelene@gmail.com>
2018-10-19 09:13:33 +0200

    setup(Prisma): add Prisma config for db and service handling

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # graphql-js-starter
+
+## Start the project
+
+### Clone the project
+
+```
+cd <your_workspace>/
+git clone https://github.com/mariehmai/graphql-js-starter.git
+cd graphql-js-starter/
+```
+
+### Run the project
+
+```
+node src/index.js     // Runs GraphQL server
+graphql playground    // Opens both app and service API playgrounds
+```
+
+## Contribute
+
+### Prerequisites
+
+```
+yarn global add graphql-cli
+yarn global add prisma
+```
--- src/index.js
+++ src/index.js
@@ -1,42 +1,41 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-let events = [
-  {
-    id: 'event-1',
-    name: 'Semi-finals Volleyball World Cup',
-    location: 'Troyes, FR'
-  },
-  {
-    id: 'event-2',
-    name: 'Finals Volleyball World Cup',
-    description: 'Finals France - Croatia',
-    location: 'Bruxelles, BE'
-  }
-]
-
-let idCount = events.length
+const { Prisma } = require('prisma-binding')
 
 const resolvers = {
   Query: {
     info: () => `GraphQL JS Starter`,
-    event: () => events
+    event: (root, args, context, info) => context.db.query.events({}, info)
   },
   Mutation: {
-    post: (root, args) => {
-      const event = {
-        id: `event-${idCount++}`,
-        name: args.name,
-        description: args.description,
-        location: args.location
-      }
-      events.push(event)
-      return event
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink(
+        {
+          data: {
+            name: args.name,
+            description: args.description,
+            location: args.location
+          }
+        },
+        info
+      )
     }
   }
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  resolverValidationOptions: {
+    requireResolversForResolveType: false
+  },
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/marie-helene-mai-655e43/database/dev',
+      secret: 'mysecret123',
+      debug: true
+    })
+  })
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
